49e807d8ada8282c659d8dac6e3de92f188ee37e
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2024-06-12 14:05:29 +0530

    login done

---
+++ Controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require('../models/userModel');
+const expressAsyncHandler = require('express-async-handler');
+const generateToken = require("../config/generatetoken");
+
+
+const loginSignup=expressAsyncHandler(async (req, res) => {
+    const { username, password } = req.body;
+    const user=await User.findOne({username});
+    if(user && (await user.matchPassword(password))){
+       
+        res.json({
+            id:user._id,
+            remark:"login successfull",
+            username:user.username,
+            token:generateToken(user._id),
+        });
+        return;
+    }
+    if(user){
+        res.json({
+            remark:"Invalid password",
+        });
+        return;
+    }
+    // signup
+    const new_user=new User({
+        username,
+        password,
+    });
+    await new_user.save();
+    res.json({
+        id:new_user._id,
+        remark:"signup successfull",
+        username:new_user.username,
+        token:generateToken(new_user._id),
+    });
+
+})
+
+module.exports=loginSignup;
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const {default: mongoose} = require('mongoose');
+const expressAsyncHandler = require('express-async-handler');
+const bodyParser = require('body-parser');
+const loginSignup = require('./Controllers/userController');
+
+const app = express();
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+connectDb();
+
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+
+app.post('/login',loginSignup );
+
+const PORT = process.env.PORT || 5000;
+const server=app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
