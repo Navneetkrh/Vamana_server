83cbcf50ad6bf9125c22344d66304bb06178e32a
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2024-06-16 11:40:33 +0530

    Update cron.js to log API URL in the cron job

--- cron.js
+++ cron.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 function setupCronJob(apiUrl) {
   // Schedule a task to run every 5 minutes
   cron.schedule('*/5 * * * *', () => {
-    console.log('Running a task every 5 minutes');
+    console.log('Running a task every 5 minutes on');
+    // log api url
+    console.log(`API URL: ${apiUrl}`);
     axios.get(apiUrl)
       .then(response => console.log(`Response from API: ${response.statusText}`))
       .catch(error => console.error(`Error calling API: ${error.message}`));
